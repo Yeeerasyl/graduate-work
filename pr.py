36a4198fe85c6270b99c67c9c9a730b95be58e70
yerasyl_ualikhan <yeeerrraaasyl@icloud.com>
2024-05-05 13:41:46 +0600

    обновленный код для обнаружения (pr.py)

--- pr.py
+++ pr.py
@@ -1,8 +1,18 @@
 from ultralytics import YOLO
-from PIL import Image
 import cv2
+import re
 
 model = YOLO("best.pt")
 
-im1 = cv2.imread("www.jpg")
-results = model.predict(source=im1) 
+im1 = cv2.imread("qqq.jpg")
+results = model.predict(source=im1)
+
+final_result = results[0].verbose()
+
+predictions_list = final_result.split(", ")
+first_prediction = predictions_list[0]
+
+first_prediction = re.sub(r'[\d\.]+', '', first_prediction)
+
+print(first_prediction)
+
