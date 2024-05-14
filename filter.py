62864d466e8d098082e9579b7570aa283f62088f
yerasyl_ualikhan <yeeerrraaasyl@icloud.com>
2024-05-14 10:20:12 +0600

    страница поддержки

--- filter.py
+++ filter.py
@@ -6,24 +6,20 @@ def split_dataset(dataset_path, output_path, train_ratio=0.8):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    # Получаем список папок с животными внутри папки dataset
     animal_folders = [f for f in os.listdir(dataset_path) if os.path.isdir(os.path.join(dataset_path, f))]
 
     for products_folder in animal_folders:
         products_path = os.path.join(dataset_path, products_folder)
 
-        # Получаем список фотографий внутри папки с животным
         photos = [f for f in os.listdir(products_path) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
 
         shuffle(photos)
 
-        # Вычисляем количество фотографий для train и test
         num_train = int(len(photos) * train_ratio)
         train_photos = photos[:num_train]
         test_photos = photos[num_train:]
 
-        # Создаем папки train и test внутри новой папки для текущего животного
         train_path = os.path.join(output_path, 'train', products_folder)
         test_path = os.path.join(output_path, 'test', products_folder)
 
